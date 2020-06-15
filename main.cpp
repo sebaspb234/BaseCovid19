2a6a16e6cad08e425ad5b000d1f5e55330509075
sebaspb234 <62064772+sebaspb234@users.noreply.github.com>
2020-06-15 17:50:27 -0500

    Avance

--- main.cpp
+++ main.cpp
@@ -4,43 +4,71 @@
 #include "Poblacion.h"
 #include "Vulnerable.h"
 #include "NoVulnerable.h"
+#include <stdlib.h>
+#include <fstream>
 using namespace std;
 
 void imprimirLista()
 {
-    cout<<"a.Datos del paciente"<<endl;
-    cout<<"b.Sintomas"<<endl;
-    cout<<"c.Antecedentes"<<endl;
-    cout<<"d.Personal medico"<<endl;
+    cout<<"1.Datos del paciente"<<endl;
+    cout<<"2.Sintomas"<<endl;
+    cout<<"3.Antecedentes"<<endl;
+    cout<<"4.Personal medico"<<endl;
+    cout<<"5.SALIR"<<endl;
 }
+
+void menu();
 
 int main()
 {
+    menu();
+    return 0;
+}
+
+void menu(){
     Poblacion paciente;
     Vulnerable vulnerable;
     NoVulnerable noVulnerable;
-    string eleccion;
-    string verificacion;
-    imprimirLista();
-    getline(cin,eleccion);
-    if(eleccion=="a")
-    {   cout<<"El paciente tiene mas de 60 anios?   SI/NO \n";
-        getline(cin,verificacion);
-        if(verificacion=="SI"||verificacion=="Si"||verificacion=="si")
+        int eleccion;
+        string verificacion;
+        do
         {
-                vulnerable.Registro();
-                vulnerable.mostrarDatos();
-                vulnerable.centroRecomendado();
-        }
-        else if(verificacion=="NO"||verificacion=="No"||verificacion=="no")
-        {
-            noVulnerable.Registro();
-            noVulnerable.mostrarDatos();
-            //noVulnerable.mostrarVector();
-        }
+            imprimirLista();
+            cin>>eleccion;
+            cin.ignore();
+            system("cls");
+            if(eleccion==1)
+            {
+                cout<<"1.Mostrar pacientes registrados\n";
+                cout<<"2.Resgistrar paciente\n";
+                cin>>eleccion;
+                cin.ignore();
+                if(eleccion==1)
+                {
+                    vulnerable.mostrar_Registro();
+                }
+                if(eleccion==2){
+                    cout<<"El paciente tiene mas de 60 anios?   SI/NO \n";
+                    getline(cin,verificacion);
+                    system("cls");
+                    if(verificacion=="SI"||verificacion=="Si"||verificacion=="si")
+                    {
+                            vulnerable.Registro(); system("cls");
+                            vulnerable.mostrarDatos();
+                            vulnerable.registro_Paciente();
+                            vulnerable.centroRecomendado();
+                            menu();
+                    }
+                    else if(verificacion=="NO"||verificacion=="No"||verificacion=="no")
+                    {
+                        noVulnerable.Registro();
+                        noVulnerable.mostrarDatos();
+                    }
+                }
+            }
+            else if(eleccion==2){paciente.Sintomas();}
+            else if(eleccion==3){paciente.AntecedentesMedicos();}
+            else if(eleccion==5){return;}
+        }while(eleccion!=1||eleccion!=2||eleccion!=3||eleccion!=4);
     }
-    else if(eleccion=="b"){paciente.Sintomas();}
-    else if(eleccion=="c"){paciente.AntecedentesMedicos();}
 
-    return 0;
-}
