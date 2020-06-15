02747ae56a767bdc562cfdbe529a85af59c1e532
sebaspb234 <62064772+sebaspb234@users.noreply.github.com>
2020-06-15 17:51:46 -0500

    Avance

--- src/Vulnerable.cpp
+++ src/Vulnerable.cpp
@@ -1,6 +1,7 @@
 #include "Vulnerable.h"
 #include <iostream>
 #include <string.h>
+#include <fstream>
 using namespace std;
 
 
@@ -23,4 +24,37 @@ string Vulnerable::getCentroMedico()
 string Vulnerable::centroRecomendado()
 {
     cout<<"\nEl paciente debe asistir al Hospital Goyeneche\n";
+}
+
+void Vulnerable::registro_Paciente()
+{
+    ofstream archivo;
+    archivo.open("Pacientes_Vulnerables",ios::app|ios::out);
+    if(archivo.fail()){cout<<"No pudo abrirse el archivo"; exit(1);}
+    archivo<<nombrePaciente<<" "<<apellidoPaciente<<" "<<edad<<" "<<distrito<<endl;
+    archivo.close();
+}
+
+void Vulnerable::mostrar_Registro()
+{
+    ifstream leerfile;
+    leerfile.open("Pacientes_Vulnerables",ios::in);
+    if(leerfile.is_open())
+    {
+        cout<<"Registro de paciente"<<endl;
+        leerfile>>nombrePaciente;
+        while(!leerfile.eof())
+        {
+            leerfile>>apellidoPaciente;
+            leerfile>>edad;
+            leerfile>>distrito;
+            cout<<"Nombre: "<<nombrePaciente<<endl;
+            cout<<"Apellido: "<<apellidoPaciente<<endl;
+            cout<<"Edad: "<<edad<<endl;
+            cout<<"Distrito: "<<distrito<<endl;
+            leerfile>>nombrePaciente;
+        }
+        leerfile.close();
+    }else{cout<<"El archivo no pudo abrirse\n";}
+
 }
